03571c78e231aa47014709ef105e5da36b7c5fbe
batu <ba921@nyu.edu>
2017-10-09 16:36:23 -0700

    final upload

--- submission/p1.py
+++ submission/p1.py
@@ -1,3 +1,6 @@
+# Nichole Lasater(nlasater) & Batu Aytemiz (baytemiz)
+# Game AI Project 1
+
 from p1_support import load_level, show_level, save_level_costs
 from math import inf, sqrt
 from heapq import heappop, heappush
@@ -225,10 +228,10 @@ def cost_to_all_cells(filename, src_waypoint, output_filename):
 
 
 if __name__ == '__main__':
-    filename, src_waypoint, dst_waypoint = '../input/my_maze.txt', 'a','d'
+    filename, src_waypoint, dst_waypoint = 'my_maze.txt', 'a','d'
 
     # Use this function call to find the route between two waypoints.
     test_route(filename, src_waypoint, dst_waypoint)
 
     # Use this function to calculate the cost to all reachable cells from an origin point.
-    cost_to_all_cells(filename, src_waypoint, 'my_costs.csv')
+    cost_to_all_cells(filename, src_waypoint, 'my_maze_costs.csv')
---
+++ submission/test_p1.py
@@ -0,0 +1,39 @@
+import unittest
+import  p1
+
+class Test_P1(unittest.TestCase):
+
+        src_waypoint, dst_waypoint = "a", "e"
+        def test_trivial(self):
+            filename = "unit_test_mazes/test_trivial.txt"
+            path = p1.unit_test_route(filename, self.src_waypoint, self.dst_waypoint)
+            correct_path = [(1,1)]
+
+            path.sort()
+            correct_path.sort()
+            self.assertEqual(path, correct_path)
+
+
+        def test_simple(self):
+            filename = "unit_test_mazes/test_simple.txt"
+            path = p1.unit_test_route(filename, self.src_waypoint, self.dst_waypoint)
+            correct_path = [(1,1), (1,2), (1,3), (2,4)]
+
+            path.sort()
+            correct_path.sort()
+            self.assertEqual(path, correct_path)
+
+
+        def test_no_path(self):
+            filename = "unit_test_mazes/test_nopath.txt"
+            path = p1.unit_test_route(filename, self.src_waypoint, self.dst_waypoint)
+            correct_path = []
+
+            path.sort()
+            correct_path.sort()
+            self.assertEqual(path, correct_path)
+
+
+
+if __name__ == '__main__':
+        unittest.main()
